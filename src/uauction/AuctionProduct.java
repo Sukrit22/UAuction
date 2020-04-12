f6f458d401fe5dbeeb9669f9843083350e7fc59f
Sukrit22 <popyoy7@gmail.com>
2020-04-12 22:00:32 +0700

    Initial commit

---
+++ src/uauction/AuctionProduct.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uauction;
+
+/**
+ *
+ * @author Sukrit22
+ */
+public class AuctionProduct {
+    private javafx.scene.image.Image mainImage;
+    private java.util.ArrayList<javafx.scene.image.Image> additionalImage;
+    
+    private String name;
+    private String description;
+    
+    private 
+}
