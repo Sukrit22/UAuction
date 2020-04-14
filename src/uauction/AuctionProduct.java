842b00fc5e4a81e70f3358398f1b743182d0b163
teethat-lsk <63555111+teethat-lsk@users.noreply.github.com>
2020-04-14 23:49:39 +0700

    newBE

--- src/uauction/AuctionProduct.java
+++ src/uauction/AuctionProduct.java
@@ -5,7 +5,17 @@
  */
 package uauction;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.image.Image;
 
 /**
  *
@@ -14,6 +24,9 @@ import java.util.Date;
 public class AuctionProduct {
     private javafx.scene.image.Image mainImage;
     //private java.util.ArrayList<javafx.scene.image.Image> additionalImage;
+    
+    private File ImagePath = new File(System.getProperty("user.dir")+"/AuctionDataBase/ProductImage/"+this.name+".png");
+    private Image image;
     
     private String name;
     private String description;
@@ -24,14 +37,80 @@ public class AuctionProduct {
     private java.util.Date datePosted;
     private java.util.Date dateEndBid;
     
+    private boolean registable = false;
     
-    AuctionProduct(String name,String descriotion,double startingBid,double minimumBid,long durationInMin){
+            
+            
+    public AuctionProduct(String name,String catalog,String descriotion,double startingBid,double minimumBid,long durationInMin){
+        if(registerProduct()){
         this.name = name;
         this.description = descriotion;
         this.startingBid = startingBid;
         this.minimumBid = minimumBid;
         datePosted = new Date();
         dateEndBid.setTime(datePosted.getTime()+ durationInMin*60*1000);
+        registable = true;
+        }
+        else
+        {
+            registable = false;
+        }
+        
         //mainImage = new javafx.scene.image.Image();
     }
+    
+    public Boolean registerProduct()
+    {
+        boolean registed = false;
+        try 
+        {
+            FileOutputStream reader = new FileOutputStream(ImagePath);
+            try 
+            {
+                ObjectOutputStream objReader = new ObjectOutputStream(reader);
+            } 
+            catch (IOException ex) 
+            {
+                System.out.println(ex.toString());
+            }
+            
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            System.out.println(ex.toString());
+        }
+        
+        return registed;
+    }
+    
+    public void setImage()
+    {
+        try 
+        {
+            FileInputStream input = new FileInputStream(ImagePath);
+            image = new Image(input);
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public Date getDateposted()
+    {
+        return datePosted;
+    }
+    public Date getDateEndBid()
+    {
+        return dateEndBid;
+    }
+    
 }
