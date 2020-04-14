832801bb4c7b397e773bffe96b30bb2aa68a5ea8
Sukrit22 <popyoy7@gmail.com>
2020-04-15 00:34:22 +0700

    เอาclass database กับ ตัวregisterProขึ้น

---
+++ src/uauction/Database.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uauction;
+
+/**
+ *
+ * @author Sukrit22
+ */
+public class Database {
+
+    /** keep registered product
+     *
+     */
+    public static java.util.Map<String,String> fileNameOfItemId;
+    
+    public static java.util.Map<String,String> fileNameOf;
+}
