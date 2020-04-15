74c62748dd6b0f00645f66127790ca36636f1d13
Sukrit22 <popyoy7@gmail.com>
2020-04-15 16:34:38 +0700

    สร้าง ManageProduct class
    เอามาใช้เป็นระบบจัดการต่างๆเกี่ยวกับสินค้า
    โดยใช้ManageProduct เป็นตัวจัดการทั้งหมด

---
+++ src/uauction/ManageProduct.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uauction;
+
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author Sukrit22
+ */
+public class ManageProduct {
+    public ManageProduct(){
+        
+    }
+    
+    /**@Deprecated
+     * register product to database and add product to list that keep all product in server
+     * take what AuctionProduct needs and registrant's username
+     * 
+     * @return whether the register is fail or succeed
+     */
+    public static boolean register(String name, String description, String image, Double startingBid, Double minimumBid){
+        
+        
+        return false;
+    }
+    
+}
