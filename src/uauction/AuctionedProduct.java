9d1a181cd7ef2df9a031cdae921c0721a6687d5e
Sukrit22 <popyoy7@gmail.com>
2020-04-15 16:33:14 +0700

    สร้าง AuctionedProduct class
    ไว้ให้ database keeptrack สินค้าที่ถูกประมูลไปแล้ว

---
+++ src/uauction/AuctionedProduct.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uauction;
+
+/**
+ *
+ * @author Sukrit22
+ */
+public class AuctionedProduct {
+    /**
+     * this product
+     */
+    public AuctionProduct auctionProduct;
+    /**
+     * the most money bidded on this product
+     */
+    public double maxBid;
+    /**
+     * Who win the auction of this product
+     */
+    public String username;
+}
