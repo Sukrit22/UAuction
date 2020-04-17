39a07fbaee106b5e900f14f248f31ab83d2b7052
teethat-lsk <63555111+teethat-lsk@users.noreply.github.com>
2020-04-18 01:14:45 +0700

    new Product constructor && imagemanagement in Productclass

--- src/uauction/Product.java
+++ src/uauction/Product.java
@@ -5,6 +5,7 @@
  */
 package uauction;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,12 +17,14 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.image.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author Sukrit22
  */
-public class Product implements java.io.Serializable {
+public final class Product implements java.io.Serializable {
     //private java.util.ArrayList<javafx.scene.image.Image> additionalImage;
     
     private String imageName;
@@ -36,6 +39,7 @@ public class Product implements java.io.Serializable {
     private java.util.Date datePosted;
     private java.util.Date dateEndBid;
     
+    private ImageIcon image;
             
             
     public Product(String name, String description, String imageName, double startingBid, double minimumBid,Date endDate){
@@ -46,8 +50,41 @@ public class Product implements java.io.Serializable {
         this.startingBid = startingBid;
         this.minimumBid = minimumBid;
         this.imageName = imageName;
+        image = new ImageIcon(System.getProperty("user.dir")+"/AuctionDataBase/Image/"+ this.getImageName()+ ".jpg");
+        
     }
-
+    public Product(String name, String description, String imageName,String initImagePath, double startingBid, double minimumBid,Date endDate){
+    
+        datePosted = new Date();
+        dateEndBid = endDate;
+        this.name = name;
+        this.description = description;
+        this.startingBid = startingBid;
+        this.minimumBid = minimumBid;
+        this.imageName = imageName;
+        writeImportedImageFile(initImagePath);
+        image = new ImageIcon(System.getProperty("user.dir")+"/AuctionDataBase/Image/"+ this.getImageName()+ ".jpg");
+    }
+    
+    
+    public void writeImportedImageFile(String initPath){
+        
+        File initImage = new File(initPath);
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(initImage);
+            ImageIO.write(image,"jpg",new File(System.getProperty("user.dir")+"/AuctionDataBase/Image/"+ this.getImageName()+ ".jpg"));
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public void setImage(String imageName)
+    {
+            
+        
+    }
+    
     public String getName(){
         return name;
     }
@@ -77,5 +114,6 @@ public class Product implements java.io.Serializable {
     public double getMinimumBid() {
         return minimumBid;
     }
+   
     
 }
