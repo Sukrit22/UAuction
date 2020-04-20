c57afc0f04d1d6455c756bd84256fbbae7e00636
teethat-lsk <63555111+teethat-lsk@users.noreply.github.com>
2020-04-21 00:41:36 +0700

    new server&client

---
+++ src/uauction/Client.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uauction;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**Client is a class that ..
+ *
+ * @author Sukrit22
+ */
+public class Client extends Application {
+
+//======================= data field =============================
+
+    //dataField to use in program
+
+//======================= Constructor ============================
+
+    public static void main(String[] args) {
+        //start program here
+        //in order of loading all local data and open the gui then request the data from server
+        //that point on this program is gonna be event driven program, means we put action on ButtonEvent.java
+        
+        //load local data
+        
+        
+        //start gui
+        
+        
+    }
+
+//======================= method =================================
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //start gui in here
+    }
+
+
+
+}
