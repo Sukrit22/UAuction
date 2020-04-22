90bd1e3122986164218e58a3783666073955d6ab
teethat-lsk <63555111+teethat-lsk@users.noreply.github.com>
2020-04-23 05:07:40 +0700

    ...

--- src/uauction/NewClient.java
+++ src/uauction/NewClient.java
@@ -36,7 +36,7 @@ public class NewClient
     
     
     public static void main(String[] args) throws Exception {
-        server = new Socket("171.6.209.126",1233);
+        
         user = new User();
         AuctionMain.AuctionMain.main(args);
 //        while(true)
@@ -54,29 +54,52 @@ public class NewClient
 //    }
     }
     
-    public static Object reqLogin(String username,String password) throws IOException, ClassNotFoundException
+    public static Object reqLogin(String username,String password) throws ClassNotFoundException, IOException
     {
-        ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream());
-        toServer.writeObject(new String("Login" + " " + username + " " + password));
+        server = new Socket("171.6.209.126",1234);
+        ObjectOutputStream toServer = null;
+        try{
+        toServer = new ObjectOutputStream(server.getOutputStream()); 
+        toServer.writeObject(("Login" + " " + username + " " + password));
+        }catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+       
         //PrintWriter toServer = new PrintWriter(server.getOutputStream(),true);
         //toServer.println("Login"+" "+username+" "+password);
         
-        ObjectInputStream fromServer = new ObjectInputStream(server.getInputStream());
-        return fromServer.readObject();
+        ObjectInputStream fromServer = null;
+        try {
+            fromServer = new ObjectInputStream(server.getInputStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        Object obj =null;
+        try {
+            obj= fromServer.readObject();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        server.close();
+        return obj;
     }
     
     public static Object reqRegister(String username,String password) throws IOException, ClassNotFoundException
     {
-        ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream());
-        toServer.writeObject(new String("Register" + " " + username + " " + password));
-        toServer.flush();
-        toServer.close();
+        server = new Socket("171.6.209.126",1234);
+        try (ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream())) {
+            toServer.writeObject(new String("Register" + " " + username + " " + password));
+            toServer.flush();
+        }
         
         ObjectInputStream fromServer = new ObjectInputStream(server.getInputStream());
         return fromServer.readObject();
+        server.close();
     }
     public static void reqRegisterProduct (Product product,BufferedImage image) throws IOException
     {
+        server = new Socket("171.6.209.126",1234);
         ImPr impr = new ImPr(product,image);
         
         ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream());
@@ -86,26 +109,30 @@ public class NewClient
         toServer2.writeObject(impr);
         toServer2.flush();
         toServer2.close();
+        server.close();
     }
     public static Object reqProduct(String fileName/*product.getFilename()*/)throws Exception
     {
-        
+        server = new Socket("171.6.209.126",1234);
         ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream());
         toServer.writeObject(new String("LoadProduct"+" "+fileName));
         
         ObjectInputStream fromServer = new ObjectInputStream(server.getInputStream());
         Product product  = (Product)fromServer.readObject();
-        
+        server.close();
         return product;
     }
     public static void reqBid(String productName/*product.getName*/,String cost,String bidderName/*User.getName*/) throws Exception
     {
+        server = new Socket("171.6.209.126",1234);
         ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream());
         toServer.writeObject(new String("Bid"+" "+cost+" "+bidderName));
+        server.close();
     }
     
     public static void reqMarket () throws Exception
     {
+        server = new Socket("171.6.209.126",1234);
         ObjectOutputStream toServer = new ObjectOutputStream(server.getOutputStream());
         toServer.writeObject(new String("Market"+" "));
         toServer.flush();
@@ -119,7 +146,7 @@ public class NewClient
             reqImage(ap.getProduct().getImageName());
             
         }
-        
+        server.close();
     }
     
     public static void reqImage(String imageName) throws Exception
