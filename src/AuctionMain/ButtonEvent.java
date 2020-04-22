72c6dc28016d7e3cb29bd8198cb50940503c37b5
Sukrit22 <popyoy7@gmail.com>
2020-04-23 04:05:10 +0700

    Update ButtonEvent.java

--- src/AuctionMain/ButtonEvent.java
+++ src/AuctionMain/ButtonEvent.java
@@ -123,20 +123,20 @@ public class ButtonEvent {
         EventHandler<ActionEvent> add = (ActionEvent ActionEvent) -> {
             System.out.println("Add from MyACC");
             Date end = Date.from((AddProduct.datePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
-            long hour= (long)AddProduct.comboBoxHour.getItems().indexOf(AddProduct.comboBoxHour.getValue());
-            hour = hour *60 *60*1000;
-            if(AddProduct.comboBoxAmPm.getItems().indexOf(AddProduct.comboBoxAmPm.getValue()) == 1 ){
-                hour += (long)(43200000);
+            long hour = (long) AddProduct.comboBoxHour.getItems().indexOf(AddProduct.comboBoxHour.getValue());
+            hour = hour * 60 * 60 * 1000;
+            if (AddProduct.comboBoxAmPm.getItems().indexOf(AddProduct.comboBoxAmPm.getValue()) == 1) {
+                hour += (long) (43200000);
             }
-            end.setTime((long) (end.getTime()+hour));
+            end.setTime((long) (end.getTime() + hour));
             String name = AddProduct.productName.getText();
-                Product product = new Product(name, AddProduct.productDescription.getText(), AddProduct.getSelectText().getText(), Double.parseDouble(AddProduct.startBid.getText()),Double.parseDouble(AddProduct.minimumBid.getText()), end);
+            Product product = new Product(name, AddProduct.productDescription.getText(), AddProduct.getSelectText().getText(), Double.parseDouble(AddProduct.startBid.getText()), Double.parseDouble(AddProduct.minimumBid.getText()), end);
 
             //NewClient.reqRegisterProduct(product, image);
             CategorisePane.vboxArray.get(0).getChildren().add(ProductPaneInVbox.Pane1());
         };
         PaneMyAccount.btnAdd.setOnAction(add);
-
+//=========================== done =======================
         EventHandler<ActionEvent> delete = (ActionEvent ActionEvent) -> {
             if(!CategorisePane.vboxArray.get(0).getChildren().isEmpty())
             CategorisePane.vboxArray.get(0).getChildren().remove(CategorisePane.vboxArray.get(0).getChildren().size()-1);
@@ -157,7 +157,7 @@ public class ButtonEvent {
 
         };
         CreateButton.buttonPopSwitchToSignUp.setOnMouseClicked(buttonPopSwitchToSignUpEV);
-
+//=========================== done =======================
         EventHandler<MouseEvent> buttonPopUpSwitchToLogInEV = (MouseEvent ActionEvent) -> {
             //Do code here
             CreateButton.buttonPopSwitchToSignUp.setVisible(true);
@@ -171,10 +171,9 @@ public class ButtonEvent {
             PopUp.passwordNotSamePane.setVisible(false);
             PopUp.emailUsedPane.setVisible(false);
             System.out.println("buttonPopUpSwitchToLogIn");
-
         };
         CreateButton.buttonPopUpSwitchToLogIn.setOnMouseClicked(buttonPopUpSwitchToLogInEV);
-
+//=========================== done =======================
         EventHandler<MouseEvent> buttonLogInPaneTopEV = (MouseEvent ActionEvent) -> {
             //Do code here
             System.out.println("buttonLogInPaneTop");
@@ -192,10 +191,9 @@ public class ButtonEvent {
             PopUp.passwordNotSamePane.setVisible(false);
             PopUp.emailUsedPane.setVisible(false);
             SceneHomeUnLogIn.getStackPane().getChildren().add(PopUp.getStackPane());
-
         };
         CreateButton.buttonLogInPaneTop.setOnMouseClicked(buttonLogInPaneTopEV);
-
+//=========================== done =======================
         EventHandler<MouseEvent> signOut = (MouseEvent ActionEvent) -> {
             //Do code here
             CreateButton.buttonHelpPaneTop.setLayoutX(1920 - 400 - 150);
@@ -214,19 +212,18 @@ public class ButtonEvent {
             PaneTop.getPane().getChildren().remove(CreateButton.buttonMyAccount);
             PaneTop.getPane().getChildren().remove(CreateButton.buttonSignOutTopPane);
             PaneTop.getPane().getChildren().add(CreateButton.buttonLogInPaneTop);
-
+            NewClient.user = new User();
         };
         CreateButton.buttonSignOutTopPane.setOnMouseClicked(signOut);
-
+//=========================== done =======================
         EventHandler<MouseEvent> myAcc = (MouseEvent ActionEvent) -> {
             //Do code here
             System.out.println("My Account");
             PaneTop.getPane().getChildren().remove(CreateButton.buttonMyAccount);
             SceneHomeUnLogIn.getStackPane().getChildren().add(PaneMyAccount.getPaneMyAcclayer1());
         };
-
         CreateButton.buttonMyAccount.setOnMouseClicked(myAcc);
-
+//=========================== done =======================
         EventHandler<ActionEvent> back = (ActionEvent ActionEvent) -> {
             PaneTop.getPane().getChildren().add(CreateButton.buttonMyAccount);
             SceneHomeUnLogIn.getStackPane().getChildren().remove(PaneMyAccount.getPaneMyAcclayer1());
@@ -246,7 +243,7 @@ public class ButtonEvent {
         };
         PaneMyAccount.btnBack.setOnAction(back);
         PaneMyAccount.btnBack2.setOnAction(back);
-        
+        //=========================== done =======================
         EventHandler<MouseEvent> backMVE = (MouseEvent ActionEvent) -> {
             PaneTop.getPane().getChildren().add(CreateButton.buttonMyAccount);
             SceneHomeUnLogIn.getStackPane().getChildren().remove(PaneMyAccount.getPaneMyAcclayer1());
@@ -265,7 +262,7 @@ public class ButtonEvent {
         };
         CreateButton.buttonBackOnACC.setOnMouseClicked(backMVE);
         CreateButton.buttonBackOnACC2.setOnMouseClicked(backMVE);
-        
+        //=========================== done =======================
         EventHandler<MouseEvent> notifi = (MouseEvent ActionEvent) -> {
             System.out.println("Notifi");
             SceneHomeUnLogIn.getStackPane().getChildren().add(Notifications.pane);
