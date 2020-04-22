da2cba71be1fcc53d7f4b44af43c8a58faf70c02
Sukrit22 <popyoy7@gmail.com>
2020-04-22 15:30:52 +0700

    multi Thread server serverคุยกับclientได้หลายตัวพร้อมกัน

---
+++ src/TestSukrit/ServerClientThread.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TestSukrit;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+/**ServerClientThread is a class that ..
+ *
+ * @author Sukrit22
+ */
+class ServerClientThread extends Thread {
+  Socket serverClient;
+  int clientNo;
+  int squre;
+  ServerClientThread(Socket inSocket,int counter){
+    serverClient = inSocket;
+    clientNo=counter;
+  }
+  public void run(){
+    try{
+      DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+      DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
+      String clientMessage="", serverMessage="";
+      while(!clientMessage.equals("bye")){
+        clientMessage=inStream.readUTF();
+        System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
+        squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
+        serverMessage="From Server to Client-" + clientNo + " Square of " + clientMessage + " is " +squre;
+        outStream.writeUTF(serverMessage);
+        outStream.flush();
+      }
+      inStream.close();
+      outStream.close();
+      serverClient.close();
+    }catch(Exception ex){
+      System.out.println(ex);
+    }finally{
+      System.out.println("Client -" + clientNo + " exit!! ");
+    }
+  }
+}
---
+++ src/TestSukrit/TCPClient.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TestSukrit;
+
+/**TCPClient is a class that ..
+ *
+ * @author Sukrit22
+ */
+import java.net.*;
+import java.io.*;
+public class TCPClient {
+  public static void main(String[] args) throws Exception {
+  try{
+    Socket socket=new Socket("127.0.0.1",8888);
+    DataInputStream inStream=new DataInputStream(socket.getInputStream());
+    DataOutputStream outStream=new DataOutputStream(socket.getOutputStream());
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    String clientMessage="",serverMessage="";
+    while(!clientMessage.equals("bye")){
+      System.out.println("Enter number :");
+      clientMessage=br.readLine();
+      outStream.writeUTF(clientMessage);
+      outStream.flush();
+      serverMessage=inStream.readUTF();
+      System.out.println(serverMessage);
+    }
+    outStream.close();
+    outStream.close();
+    socket.close();
+  }catch(Exception e){
+    System.out.println(e);
+  }
+  }
+}
